83f21ec1310fd40cacd7cee65447eb2cd85d2985
xmarduel <xavier.marduel@web.de>
2022-02-26 13:11:29 +0100

    cont

---
+++ demos_code/stock_model.py
@@ -0,0 +1,29 @@
+
+'''
+
+
+
+
+
+'''
+
+class StockModel:
+    '''
+    '''
+    def __init__(self, dimX, dimY, dimZ):
+        self.dimX = dimX
+        self.dimY = dimY
+        self.dimZ = dimZ
+
+        self.resolution = 1024
+
+
+    def setup(self, path):
+        '''
+        '''
+        pass
+
+    def generate_voxel(self):
+        '''
+        '''
+        pass
